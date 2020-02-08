742e6d2bb4a326b6c48ea1a757acb6207856f507
Mathias Kegelmann <mathias.kegelmann@gmail.com>
2020-02-08 19:57:02 +0100

    minor change to MOTD

--- paolas-moodle/startup.sh
+++ paolas-moodle/startup.sh
@@ -2,7 +2,7 @@
 
 apt-get update
 apt-get -y install docker docker-compose htop
-echo >/etc/motd PLEASE remember "sudo apt-get upgrade"
+echo >/etc/motd PLEASE don\'t forget to \"sudo apt-get upgrade\"
 
 if ! id -u mathias; then adduser --disabled-password --gecos "" mathias; fi
 if ! id -u paola; then adduser --disabled-password --gecos "" paola; fi
