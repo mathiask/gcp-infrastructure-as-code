6ac87a7a558f5edecffca512003a7a5ada6f743b
Mathias Kegelmann <mathias.kegelmann@gmail.com>
2020-02-10 14:16:09 +0100

    added ddns hostname to MOTD

--- paolas-moodle/startup.sh
+++ paolas-moodle/startup.sh
@@ -2,7 +2,8 @@
 
 apt-get update
 apt-get -y install docker docker-compose htop
-echo >/etc/motd PLEASE don\'t forget to \"sudo apt-get upgrade\"
+echo >/etc/motd External host name: paolas-moodle.ddnsfree.com
+echo >>/etc/motd PLEASE don\'t forget to \"sudo apt-get upgrade\"!
 
 if ! id -u mathias; then adduser --disabled-password --gecos "" mathias; fi
 if ! id -u paola; then adduser --disabled-password --gecos "" paola; fi
